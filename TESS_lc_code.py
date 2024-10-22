706b3180bad3c4517bde51495e58a5689d9e9e2e
jakemendelsohn <ssqm77@durham.ac.uk>
2024-10-22 18:02:04 +0100

    DW Cnc unfinished with beat frequencies plotted

--- TESS_lc_code.py
+++ TESS_lc_code.py
@@ -13,7 +13,7 @@ import lightkurve as lk
 
 
 def sector_data(index):
-    search_result = lk.search_lightcurve('EX Hydrae', mission='TESS')
+    search_result = lk.search_lightcurve('DW Cnc', mission='TESS')
     print(search_result)
     lc = search_result[index].download()
     exptime = search_result.table['exptime'][index]
@@ -42,7 +42,7 @@ def Lomb_Scargle(time,flux,exptime):
     min_freq, max_freq = frequency_range(time,flux,exptime)
     # Compute the Lomb-Scargle Periodogram within the specified frequency range
     num_frequency_points = 100000  # You can adjust this based on the desired resolution
-    frequency = np.linspace(min_freq*100, max_freq/5, num_frequency_points)
+    frequency = np.linspace(min_freq*10, max_freq/5, num_frequency_points)
     ls = LombScargle(time, flux)
     power = ls.power(frequency,normalization = 'model')# Manually compute power without autopower
     # Plot the Lomb-Scargle Periodogram
@@ -65,7 +65,8 @@ def Lomb_Scargle(time,flux,exptime):
     
     #Plot peaks#
     peak_frequencies, peak_powers = peak_finder(frequency, power)
-    orbital_frequencies, spin_frequencies,new_frequencies = peak_classification(frequency,power,peak_frequencies,peak_powers,orbital_period = 0.068233846,spin_period = 0.046546484,tolerance = 0.001)
+    #print("Peak Frequencies", peak_frequencies)
+    orbital_frequencies, spin_frequencies,new_frequencies, beat_frequencies = peak_classification(frequency,power,peak_frequencies,peak_powers)
     for freq2 in new_frequencies:
         alrm = false_alarm(ls, power,frequency, freq2)
         #print("Freq", freq2, ":", alrm)
@@ -77,6 +78,9 @@ def Lomb_Scargle(time,flux,exptime):
     for freq1 in spin_frequencies:
         x_vals = np.linspace(freq1,freq1,1000)
         plt.plot(x_vals,y_vals,linestyle = ":", color = 'red')
+    for freq2 in beat_frequencies:
+        x_vals = np.linspace(freq1,freq1,1000)
+        plt.plot(x_vals,y_vals,linestyle = ":", color = 'black')
     #print("The remaining frequency peaks are", remaining_frequencies)
     x_values = np.linspace(22.47804849975284,22.47804849975284,1000)
     plt.plot(x_values,y_vals,linestyle = ":", color = 'green')
@@ -85,10 +89,11 @@ def Lomb_Scargle(time,flux,exptime):
     plt.plot([], [], linestyle=":", color='blue', label='Orbital Frequencies')  # Add one blue line to the legend
     plt.plot([], [], linestyle=":", color='red', label='Spin Frequencies')      # Add one red line to the legend
     plt.plot([], [], linestyle=":", color='green', label='Remaining Frequencies')
+    plt.plot([], [], linestyle=":", color='black', label='Beat Frequencies')
     plt.legend()
     # Show the plot
     plt.show()
-    return frequency,power,orbital_frequencies,spin_frequencies,new_frequencies
+    return frequency,power,orbital_frequencies,spin_frequencies,new_frequencies,beat_frequencies
 
 def mulitple_sector_LS(index_list):
     results = {}
@@ -105,15 +110,15 @@ def mulitple_sector_LS(index_list):
     
     results2 = {}
     for i in range (0,len(times)):
-        frequency,power,orbital_frequencies,spin_frequencies,new_frequencies = Lomb_Scargle(times[i],fluxes[i],exptimes[i])
-        results2[f"var_{i}_1_2_3_4_5"] = (frequency,power,orbital_frequencies,spin_frequencies,new_frequencies)
+        frequency,power,orbital_frequencies,spin_frequencies,new_frequencies, beat_frequencies = Lomb_Scargle(times[i],fluxes[i],exptimes[i])
+        results2[f"var_{i}_1_2_3_4_5_6"] = (frequency,power,orbital_frequencies,spin_frequencies,new_frequencies,beat_frequencies)
 
     frequencies = [value[0] for value in results2.values()]
     powers = [value[1] for value in results2.values()]
     orbitals = [value[2] for value in results2.values()]
     spins = [value[3] for value in results2.values()]
     news = [value[4] for value in results2.values()]
-    
+    beats = [value[5] for value in results2.values()]
     
     fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(10, 12), sharex=True)
     plt.subplots_adjust(hspace=0)  # hspace=0 removes the space between the subplot
@@ -137,6 +142,9 @@ def mulitple_sector_LS(index_list):
     for freq1 in spins[0]:
         x_vals = np.linspace(freq1,freq1,1000)
         ax1.plot(x_vals,y_vals,linestyle = ":", color = 'red')
+    for freq2 in beats[0]:
+        x_vals = np.linspace(freq2,freq2,1000)
+        ax1.plot(x_vals,y_vals,linestyle = ":", color = 'black')
     #print("The remaining frequency peaks are", remaining_frequencies)
     x_values = np.linspace(22.47804849975284,22.47804849975284,1000)
     ax1.plot(x_values,y_vals,linestyle = ":", color = 'green')
@@ -149,6 +157,9 @@ def mulitple_sector_LS(index_list):
     for freq1 in spins[1]:
         x_vals = np.linspace(freq1,freq1,1000)
         ax2.plot(x_vals,y_vals,linestyle = ":", color = 'red')
+    for freq2 in beats[1]:
+        x_vals = np.linspace(freq2,freq2,1000)
+        plt.plot(x_vals,y_vals,linestyle = ":", color = 'black')
     #print("The remaining frequency peaks are", remaining_frequencies)
     x_values = np.linspace(22.47804849975284,22.47804849975284,1000)
     ax2.plot(x_values,y_vals,linestyle = ":", color = 'green')
@@ -158,6 +169,7 @@ def mulitple_sector_LS(index_list):
     plt.plot([], [], linestyle=":", color='blue', label='Orbital Frequencies')  # Add one blue line to the legend
     plt.plot([], [], linestyle=":", color='red', label='Spin Frequencies')      # Add one red line to the legend
     plt.plot([], [], linestyle=":", color='green', label='Remaining Frequencies')
+    plt.plot([], [], linestyle=":", color='black', label='Beat Frequencies')
     plt.legend()
     
     
@@ -167,7 +179,7 @@ def mulitple_sector_LS(index_list):
     
     return times, fluxes, orbitals, spins, news
 
-def peak_finder(frequency, power,  height_threshold=0.02, prominence=0.0001):
+def peak_finder(frequency, power,  height_threshold=0.02, prominence=0.01):
     y = frequency*power
     peaks, properties = find_peaks(y, height=height_threshold, prominence=prominence)
     
@@ -177,17 +189,24 @@ def peak_finder(frequency, power,  height_threshold=0.02, prominence=0.0001):
     
     return peak_frequencies, peak_powers
 
-def peak_classification(frequency,power,peak_frequencies,peak_powers,orbital_period = 0.068233846,spin_period = 0.046546484,tolerance = 0.001):
+def peak_classification(frequency,power,peak_frequencies,peak_powers,tolerance = 0.0015):
+    orbital_period = 0.05979267
+    spin_period = 0.02679429
     natural_orbital_frequency = 1/orbital_period
     natural_spin_frequency = 1/spin_period
+    natural_beat_frequency = abs(natural_spin_frequency-natural_orbital_frequency)
+    print("nat", natural_beat_frequency)
     orbital_frequencies = []
     spin_frequencies = []
+    beat_frequencies = []
     for freq in peak_frequencies:
         if abs(freq / natural_orbital_frequency - round(freq / natural_orbital_frequency)) < tolerance:
             orbital_frequencies.append(freq)
         if abs(freq / natural_spin_frequency - round(freq / natural_spin_frequency)) < tolerance:
             spin_frequencies.append(freq)
-    classified_frequencies = set(orbital_frequencies + spin_frequencies)
+        if abs(freq / natural_beat_frequency - round(freq / natural_beat_frequency)) < tolerance:
+            beat_frequencies.append(freq)
+    classified_frequencies = set(orbital_frequencies + spin_frequencies + beat_frequencies)
     
     # Filter out the frequencies that are in the classified frequencies set
     remaining_frequencies = [freq for freq in peak_frequencies if freq not in classified_frequencies]
@@ -195,13 +214,14 @@ def peak_classification(frequency,power,peak_frequencies,peak_powers,orbital_per
     tolerance2 = 0.3  # For example, consider frequencies within 0.1 c/d as "close"
     print("spin",spin_frequencies)
     print("orbital", orbital_frequencies)
+    print("beat", beat_frequencies)
     # List to store new frequencies
     new_frequencies = []
     
     # Loop through remaining frequencies
     for freq in remaining_frequencies:
         # Check if this frequency is close to any orbital or spin frequency
-        is_known = np.any(np.abs(orbital_frequencies - freq) < tolerance2) or np.any(np.abs(spin_frequencies - freq) < tolerance2)
+        is_known = np.any(np.abs(orbital_frequencies - freq) < tolerance2) or np.any(np.abs(spin_frequencies - freq) < tolerance2) or np.any(np.abs(beat_frequencies - freq) < tolerance2)
         
         # If it's not close to any known frequency, add it to new_frequencies
         if not is_known:
@@ -212,7 +232,7 @@ def peak_classification(frequency,power,peak_frequencies,peak_powers,orbital_per
             
             # Output the new frequencies
     print("New frequencies:", new_frequencies)
-    return orbital_frequencies, spin_frequencies,new_frequencies
+    return orbital_frequencies, spin_frequencies,new_frequencies, beat_frequencies
 
 def false_alarm(ls,power,frequency,specific_frequency):
     closest_idx = np.argmin(np.abs(frequency - specific_frequency))
@@ -248,11 +268,11 @@ def phase_fold_binned(time, flux, peak_frequencies):
     plt.ylabel("Flux e/s")
 
 
-indexes = [1,3]
+indexes = [3,4]
 index = 3
 times, fluxes, orbitals, spins, news = mulitple_sector_LS(indexes)
 peak_frequencies = np.array([21.486123909219167,14.656134673552952,22.4780485])
-phase_fold_binned(times[1], fluxes[1], peak_frequencies)
+#phase_fold_binned(times[1], fluxes[1], peak_frequencies)
 #time,flux,exptime = sector_data(index)
 #frequency,power,peak_frequencies,peak_powers = Lomb_Scargle(time,flux,exptime)
 #peak_classification(frequency,power,peak_frequencies,peak_powers)
