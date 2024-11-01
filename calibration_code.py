1fc0b1df78f04bb0820368c7f22b4107dce2041f
jakemendelsohn <ssqm77@durham.ac.uk>
2024-11-01 15:32:11 +0000

    Latest calibration (not working)

--- calibration_code.py
+++ calibration_code.py
@@ -110,49 +110,76 @@ def calibration(time,flux,g_band_BJD_sector, g_band_flux_sector):
     plt.ylabel("Flux (e/s)")
     plt.show()
     
-    ground_time_1st_half = g_band_BJD_sector[:len(g_band_BJD_sector)//2]
-    ground_flux_1st_half = g_band_flux_sector[:len(g_band_flux_sector)//2]
+    tess_flux_normalized = (flux - np.mean(flux)) / np.std(flux)
+    assasn_flux_normalized = (g_band_flux_sector - np.mean(g_band_flux_sector)) / np.std(g_band_flux_sector)
     
-    closest_time_values = np.array([time[np.abs(time - f).argmin()] for f in ground_time_1st_half])
+    ground_time_1st_half = g_band_BJD_sector[:len(g_band_BJD_sector)//2]
+    ground_flux_1st_half = assasn_flux_normalized[:len(assasn_flux_normalized)//2]
     closest_indices = [np.abs(time - f).argmin() for f in ground_time_1st_half]
-    matched_flux_values = flux[closest_indices]
-    print("ground",ground_flux_1st_half)
-    print(matched_flux_values)
-   # plt.scatter(ground_time_1st_half,ground_flux_1st_half,s=1)
+    matched_flux_values = tess_flux_normalized[closest_indices]
+    # plt.scatter(ground_time_1st_half,ground_flux_1st_half,s=1)
     plt.scatter(matched_flux_values,ground_flux_1st_half)
     plt.xlabel("TESS Flux")
     plt.ylabel("Ground Based Flux")
-
     # Perform a linear least-squares fit
     slope, intercept, r_value, p_value, std_err = linregress(matched_flux_values, ground_flux_1st_half)
-
     # Plot the best-fit line
     x_fit = np.array([min(matched_flux_values), max(matched_flux_values)])
     y_fit = slope * x_fit + intercept
     plt.plot(x_fit, y_fit, color="red", label=f"Fit: y = {slope:.2f}x + {intercept:.2f}")
-
-    # Show fit details
     plt.legend()
     plt.show()
 
-    # Print the fit parameters
-    print("Slope:", slope)
-    print("Intercept:", intercept)
-    print("R-squared:", r_value**2)
+    ground_time_2nd_half = g_band_BJD_sector[len(g_band_BJD_sector)//2:]
+    ground_flux_2nd_half = assasn_flux_normalized[len(assasn_flux_normalized)//2:]
+    closest_indices2 = [np.abs(time - f).argmin() for f in ground_time_2nd_half]
+    matched_flux_values2 = tess_flux_normalized[closest_indices2]
+    plt.scatter(matched_flux_values2,ground_flux_2nd_half)
+    plt.xlabel("TESS Flux")
+    plt.ylabel("Ground Based Flux")
+    # Perform a linear least-squares fit
+    slope2, intercept2, r_value2, p_value, std_err = linregress(matched_flux_values2, ground_flux_2nd_half)
+    # Plot the best-fit line
+    x_fit2 = np.array([min(matched_flux_values2), max(matched_flux_values2)])
+    y_fit2 = slope2 * x_fit2 + intercept2
+    plt.plot(x_fit2, y_fit2, color="red", label=f"Fit: y = {slope2:.2f}x + {intercept2:.2f}")
+    plt.legend()
+    plt.show()
     
-    return slope,intercept, r_value
 
-def calibrated_data(slope,intercept,time,flux):
-    flux_calibrated = []
-    for fl in flux:
+    # Print the fit parameters
+    print("Slope1:", slope)
+    print("Intercept1:", intercept)
+    print("R-squared1:", r_value**2)
+    
+    print("Slope2:", slope2)
+    print("Intercept2:", intercept2)
+    print("R-squared2:", r_value2**2)
+    
+    return slope,intercept, r_value, slope2, intercept2,r_value2
+
+def calibrated_data(slope,intercept,slope2,intercept2,time,flux):
+    flux_calibrated1 = []
+    flux_calibrated2 = []
+    mid_index = len(flux) // 2
+
+    # Split the list into two halves
+    first_half = flux[:mid_index]
+    second_half = flux[mid_index:]
+    for fl in first_half:
         fl = (fl*slope)+intercept
-        flux_calibrated.append(fl)
-    plt.scatter(time,flux,s=1)
+        flux_calibrated1.append(fl)
+    for f2 in second_half:
+        f2 = (f2*slope2)+intercept2
+        flux_calibrated2.append(f2)
+    
+    flux_total = flux_calibrated1+flux_calibrated2
+    plt.scatter(time,flux_total,s=1)
         
     
     
 
 time,flux,exptime = sector_data(2)
 g_band_BJD_sector, g_band_flux_sector = ASSASSN_data(time)
-slope,intercept,r_value = calibration(time,flux, g_band_BJD_sector, g_band_flux_sector)
-calibrated_data(slope,intercept,time,flux)
+slope,intercept,r_value,slope2,intercept2,r_value2 = calibration(time,flux, g_band_BJD_sector, g_band_flux_sector)
+calibrated_data(slope,intercept,slope2, intercept2,time,flux)
